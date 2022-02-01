df6272d9d968b0766e1378e84af137a1ae003955
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2022-02-01 08:52:30 +0300

    Add templates folder and a html fie to render

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 main = Flask (__name__)
 
-
 @main.route('/')
 def index():
-    return render_template ('main.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     main.run (debug=True)
