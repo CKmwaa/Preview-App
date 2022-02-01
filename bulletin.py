941633854028e761a3c93611ae27c57109a16a8f
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2022-02-01 12:44:13 +0300

    changed all article names to bulletin

--- bulletin.py
+++ bulletin.py
@@ -1,28 +1,29 @@
-def Articles():
-    articles =[
+def Bulletin():
+    bulletin =[
         {
             'id':1,
             'title':'Politics'            ''
             'body':'Today UDA was in, '
-        }
+        },
          {
             'id':2,
             'title':''            ''
             'body':'Today UDA was in, '
-        }
+        },
          {
             'id':3,
             'title':'Agriculture'            ''
             'body':'Today UDA was in, '
-        }
+        },
          {
             'id':4,
             'title':'ICT'            ''
             'body':'Today UDA was in, '
-        }
+        },
          {
             'id':5,
             'title':'Killings'            ''
             'body':'Today UDA was in, '
         }
     ]
+return bulletin
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+from article import Articles
 
 main = Flask (__name__)
+
+Bulletin = Bulletin ()
 
 @main.route('/')
 def index():
     return render_template('main.html')
 
-@main.route('/')
+@main.route('/about')
 def about():
     return render_template ('about.html')
+
+@main.route('/bulletin')
+def bulletin():
+    return render_template ('bulletin.html', articles =Articles)
 
 if __name__ == '__main__':
     main.run (debug=True)
