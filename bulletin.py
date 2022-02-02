528bae378f17b5a17b8ff90784ffe8b640db2704
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2022-02-02 11:00:18 +0300

    Add layout.html file and bulletins.html file

--- bulletin.py
+++ bulletin.py
@@ -1,9 +1,21 @@
-#def Bulletin():
-    bulletin=[
+def Bulletin():
+    bulletin =[
         {
-            'id':1,
-            'title':'Politics'
-        }
+            'id': 1, 
+            'title':'Article one',
+            'body':'Today in Nairobi'
+            
+        },
 
+        {'id': 2, 
+            'title':'Article two',
+            'body':'Today in Nairobi'
+        }, 
+        {
+            'id': 3, 
+            'title':'Article three',
+            'body':'Today in Nairobi'
+            
+        }
     ]
     return bulletin
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template
-#from bulletin import Bulletin
+from bulletin import Bulletin
 
 main = Flask (__name__)
 
-#bulletin = Bulletin()
+Bulletin = Bulletin()
 
 @main.route('/')
 def index():
-    return render_template(' main.html')
+    return render_template('main.html')
 
 @main.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template ('about.html')
 
-#@main.route('/bulletin')
+@main.route('/bulletin')
 def bulletin():
-    return render_template('bulletin.html', bulletin =  Bulletin)
+    return render_template ('bulletin.html', bulletin = Bulletin)
 
-#@main.route('/bulletin/<string:id>/')
-def bulletin(id):
-    return render_template('bulletin.html', id=id)
+@main.route('/bulletins/<string:id>/')
+def bulletins(id):
+    return render_template ('bulletins.html', id=id)
+
 
 if __name__ == '__main__':
-    main.run (debug=True)
+
+    main.run(debug=True)
 
 
